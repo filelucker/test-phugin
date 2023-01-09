75bf2c099db7d0095e5602649b7003ee3bc3fdd2
filelucker <filelucker@gmail.com>
2023-01-09 17:33:14 +0600

    first commit

---
+++ sdk/src/main/java/com/shurjopay/plugin/RequiredData.kt
@@ -0,0 +1,99 @@
+package com.shurjopay.plugin
+
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
+
+/**
+ * Required Data data class
+ *
+ * @author  Moniruzzaman
+ * @since   2021-08-07
+ */
+@Parcelize
+data class RequiredData(
+    var username: String?,
+    var password: String?,
+    var prefix: String?,
+    var currency: String?,
+    var amount: Double,
+    var orderId: String?,
+    var discountAmount: Double?,
+    var discPercent: Double?,
+    var customerName: String?,
+    var customerPhone: String?,
+    var customerEmail: String?,
+    var customerAddress: String?,
+    var customerCity: String?,
+    var customerState: String?,
+    var customerPostcode: String?,
+    var customerCountry: String?,
+    var returnUrl: String?,
+    var cancelUrl: String?,
+    var clientIp: String?,
+    var value1: String?,
+    var value2: String?,
+    var value3: String?,
+    var value4: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readDouble(),
+        parcel.readString(),
+        parcel.readValue(Double::class.java.classLoader) as? Double,
+        parcel.readValue(Double::class.java.classLoader) as? Double,
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    companion object : Parceler<RequiredData> {
+
+        override fun RequiredData.write(parcel: Parcel, flags: Int) {
+            parcel.writeString(username)
+            parcel.writeString(password)
+            parcel.writeString(prefix)
+            parcel.writeString(currency)
+            parcel.writeDouble(amount)
+            parcel.writeString(orderId)
+            parcel.writeValue(discountAmount)
+            parcel.writeValue(discPercent)
+            parcel.writeString(customerName)
+            parcel.writeString(customerPhone)
+            parcel.writeString(customerEmail)
+            parcel.writeString(customerAddress)
+            parcel.writeString(customerCity)
+            parcel.writeString(customerState)
+            parcel.writeString(customerPostcode)
+            parcel.writeString(customerCountry)
+            parcel.writeString(returnUrl)
+            parcel.writeString(cancelUrl)
+            parcel.writeString(clientIp)
+            parcel.writeString(value1)
+            parcel.writeString(value2)
+            parcel.writeString(value3)
+            parcel.writeString(value4)
+        }
+
+        override fun create(parcel: Parcel): RequiredData {
+            return RequiredData(parcel)
+        }
+    }
+}
