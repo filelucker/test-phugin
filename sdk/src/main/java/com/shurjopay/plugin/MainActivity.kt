a10c7d3837b23cd87821e9cc94efc8600fb52a34
filelucker <filelucker@gmail.com>
2023-01-11 15:45:43 +0600

    namespace changed

--- sdk/src/main/java/com/shurjopay/plugin/MainActivity.kt
+++ sdk/src/main/java/com/shurjopay/plugin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shurjopay.plugin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import shurjopay.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
